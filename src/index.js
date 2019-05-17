04e0b90bb0c7285cb3ddb7b74b21e001f770ee42
github username <github email address>
2019-05-17 14:59:30 -0700

    add a form for name with styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 // Import React & ReactDom libraries
 // import Variable from 'library';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; 
 
 // Create a React component
 const App = () => {
-  return <div>Hello World!</div>;
+  const buttonText = 'Click Me!';
+  // An opening tag needs to be on the same line as 'return' or you will get 'undefined'.
+  return (
+  <div>
+    <label className="label" htmlFor="name">
+      Enter Name
+      </label>
+    <input id="name" type="text" />
+    {/* <button style='background-color: blue; color: white;'>Submit</button> */}
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {buttonText}
+      </button>
+  </div>
+  );
 };
 
 // Take the react component and show on the screen
